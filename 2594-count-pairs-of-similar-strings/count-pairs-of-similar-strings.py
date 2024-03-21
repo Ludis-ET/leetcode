0b7c0ea014787ae98dc72ff88048b27d99adf0f5
Leulseged Melaku <132431582+Ludis-ET@users.noreply.github.com>
2024-03-21 15:26:42 +0300

    Time: 406 ms (19.42%) | Memory: 11.9 MB (41.75%) - LeetSync

---
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def similarPairs(self, words):
+        def is_similar(word1, word2):
+            return set(word1) == set(word2)
+        
+        count = 0
+        n = len(words)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if is_similar(words[i], words[j]):
+                    count += 1
+        return count      
