94813a4413c084a63a46efb289cde44a4664b417
Ludis <132431582+Ludis-ET@users.noreply.github.com>
2024-10-22 19:05:19 +0300

    Time: 41 ms (42.52%) | Memory: 16.4 MB (92.95%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
         arr = [0, 1]
-        for i in range(2, n):
-            arr = [arr[1], arr[0] + arr[1]]
-        return sum(arr) if n > 1 else n
+        for i in range(n - 2):
+            arr[0], arr[1] = arr[1], arr[0] + arr[1]
+        return arr[0] + arr[1] if n > 1 else n
